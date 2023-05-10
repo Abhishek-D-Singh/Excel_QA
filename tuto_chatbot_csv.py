c51e3ce72ecba62eb064b7bb1965625b4674d081
Abhishek Singh <Abhee2630@Gmail.Com>
2023-05-10 17:24:35 +0530

    Update tuto_chatbot_csv.py

--- tuto_chatbot_csv.py
+++ tuto_chatbot_csv.py
@@ -24,7 +24,8 @@ if uploaded_file :
         tmp_file_path = tmp_file.name
 
     demo_ex_loader = pd.read_excel(uploaded_file, engine = "openpyxl")
-    loader = CSVLoader(file_path=demo_ex_loader, encoding="utf-8")
+    demo_ex_loader.to_csv("file1.csv", encoding='utf-8', index=False) 
+    loader = CSVLoader(file_path="file1.csv", encoding="utf-8")
     data = loader.load()
 
     embeddings = OpenAIEmbeddings()
