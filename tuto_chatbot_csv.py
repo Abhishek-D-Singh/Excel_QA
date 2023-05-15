1c7f4cf6e95f23dffd98497bdf56ef791ae797c5
Abhishek Singh <Abhee2630@Gmail.Com>
2023-05-15 11:28:32 +0530

    Update tuto_chatbot_csv.py

--- tuto_chatbot_csv.py
+++ tuto_chatbot_csv.py
@@ -13,7 +13,6 @@ from langchain.vectorstores import FAISS
 import tempfile
 import pandas as pd
 
-
 user_api_key = st.sidebar.text_input(
     label="#### Your OpenAI API key 👇",
     placeholder="Paste your openAI API key, sk-",
@@ -26,68 +25,57 @@ if uploaded_file :
         tmp_file.write(uploaded_file.getvalue())
         tmp_file_path = tmp_file.name
 
-    demo_ex_loader = pd.read_excel(uploaded_file, engine = "openpyxl")
+    demo_ex_loader = pd.read_excel(uploaded_file, engine="openpyxl")
     demo_ex_loader.to_csv("file1.csv", encoding='utf-8', index=True) 
     loader = CSVLoader(file_path="file1.csv", encoding="utf-8")
     data = loader.load()
-    
-    
+
     index_creator = VectorstoreIndexCreator()
     docsearch = index_creator.from_loaders([loader])
 
-# Create a question-answering chain using the index
+    # Create a question-answering chain using the index
     chain = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=docsearch.vectorstore.as_retriever(), input_key="question")
 
-     
-
-# Pass a query to the chain
+    # Pass a query to the chain
     query = "Do you have a column called age?"
     response = chain({"question": query})
-     
 
     print(response['result'])
     embeddings = OpenAIEmbeddings()
     vectors = FAISS.from_documents(data, embeddings)
-    chain = ConversationalRetrievalChain.from_llm(llm = ChatOpenAI(temperature=0.0,model_name='gpt-3.5-turbo', openai_api_key=user_api_key),
-                                                                      #retriever=vectors.as_retriever())
+    chain = ConversationalRetrievalChain.from_llm(llm=ChatOpenAI(temperature=0.0, model_name='gpt-3.5-turbo', openai_api_key=user_api_key),
+                                                 retriever=vectors.as_retriever())
 
     def conversational_chat(query):
-        
         result = chain({"question": query, "chat_history": st.session_state['history']})
         st.session_state['history'].append((query, result["answer"]))
-        
         return result["answer"]
     
     if 'history' not in st.session_state:
         st.session_state['history'] = []
 
     if 'generated' not in st.session_state:
-        st.session_state['generated'] = ["Hello ! Ask me anything about " + uploaded_file.name + " 🤗"]
+        st.session_state['generated'] = ["Hello! Ask me anything about " + uploaded_file.name + " 🤗"]
 
     if 'past' not in st.session_state:
-        st.session_state['past'] = ["Hey ! 👋"]
+        st.session_state['past'] = ["Hey! 👋"]
         
-    #container for the chat history
+    # Container for the chat history
     response_container = st.container()
-    #container for the user's text input
+    # Container for the user's text input
     container = st.container()
 
     with container:
         with st.form(key='my_form', clear_on_submit=True):
-                                              
             user_input = st.text_input("Query:", placeholder="Talk about your csv data here (:", key='input')
             submit_button = st.form_submit_button(label='Send')
             
         if submit_button and user_input:
             output = conversational_chat(user_input)
-            
             st.session_state['past'].append(user_input)
             st.session_state['generated'].append(output)
 
     if st.session_state['generated']:
         with response_container:
             for i in range(len(st.session_state['generated'])):
-                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="big-smile")
-                message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
-                
-#streamlit run tuto_chatbot_csv.py
+                message(st.session_state["past"][i], is
