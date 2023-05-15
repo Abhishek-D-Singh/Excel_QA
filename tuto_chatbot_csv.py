c3fd45bb4819b0a29f4e815892407960c3042e40
Abhishek Singh <Abhee2630@Gmail.Com>
2023-05-15 11:26:50 +0530

    Update tuto_chatbot_csv.py

--- tuto_chatbot_csv.py
+++ tuto_chatbot_csv.py
@@ -26,32 +26,32 @@ if uploaded_file :
         tmp_file.write(uploaded_file.getvalue())
         tmp_file_path = tmp_file.name
 
-demo_ex_loader = pd.read_excel(uploaded_file, engine = "openpyxl")
-demo_ex_loader.to_csv("file1.csv", encoding='utf-8', index=True) 
-loader = CSVLoader(file_path="file1.csv", encoding="utf-8")
-data = loader.load()
+    demo_ex_loader = pd.read_excel(uploaded_file, engine = "openpyxl")
+    demo_ex_loader.to_csv("file1.csv", encoding='utf-8', index=True) 
+    loader = CSVLoader(file_path="file1.csv", encoding="utf-8")
+    data = loader.load()
     
     
-index_creator = VectorstoreIndexCreator()
-docsearch = index_creator.from_loaders([loader])
+    index_creator = VectorstoreIndexCreator()
+    docsearch = index_creator.from_loaders([loader])
 
 # Create a question-answering chain using the index
-chain = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=docsearch.vectorstore.as_retriever(), input_key="question")
+    chain = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=docsearch.vectorstore.as_retriever(), input_key="question")
 
      
 
 # Pass a query to the chain
-query = "Do you have a column called age?"
-response = chain({"question": query})
+    query = "Do you have a column called age?"
+    response = chain({"question": query})
      
 
-print(response['result'])
-embeddings = OpenAIEmbeddings()
-vectors = FAISS.from_documents(data, embeddings)
-chain = ConversationalRetrievalChain.from_llm(llm = ChatOpenAI(temperature=0.0,model_name='gpt-3.5-turbo', openai_api_key=user_api_key),
+    print(response['result'])
+    embeddings = OpenAIEmbeddings()
+    vectors = FAISS.from_documents(data, embeddings)
+    chain = ConversationalRetrievalChain.from_llm(llm = ChatOpenAI(temperature=0.0,model_name='gpt-3.5-turbo', openai_api_key=user_api_key),
                                                                       #retriever=vectors.as_retriever())
 
-def conversational_chat(query):
+    def conversational_chat(query):
         
         result = chain({"question": query, "chat_history": st.session_state['history']})
         st.session_state['history'].append((query, result["answer"]))
@@ -74,7 +74,7 @@ def conversational_chat(query):
 
     with container:
         with st.form(key='my_form', clear_on_submit=True):
-            
+                                              
             user_input = st.text_input("Query:", placeholder="Talk about your csv data here (:", key='input')
             submit_button = st.form_submit_button(label='Send')
             
