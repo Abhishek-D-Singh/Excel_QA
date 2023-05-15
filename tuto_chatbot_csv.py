4fc68284e124039c2b6f761100c013937f89af76
Abhishek Singh <Abhee2630@Gmail.Com>
2023-05-15 11:21:09 +0530

    Update tuto_chatbot_csv.py

--- tuto_chatbot_csv.py
+++ tuto_chatbot_csv.py
@@ -51,35 +51,38 @@ vectors = FAISS.from_documents(data, embeddings)
 chain = ConversationalRetrievalChain.from_llm(llm = ChatOpenAI(temperature=0.0,model_name='gpt-3.5-turbo', openai_api_key=user_api_key),
                                                                       #retriever=vectors.as_retriever())
 
-
-#streamlit run tuto_chatbot_csv.py
-    def conversational_chat(query):
-            result = chain({"question": query, "chat_history": st.session_state['history']})
-            st.session_state['history'].append((query, result["answer"]))
-            return result["answer"]
+def conversational_chat(query):
+        
+        result = chain({"question": query, "chat_history": st.session_state['history']})
+        st.session_state['history'].append((query, result["answer"]))
+        
+        return result["answer"]
     
     if 'history' not in st.session_state:
-            st.session_state['history'] = []
+        st.session_state['history'] = []
 
     if 'generated' not in st.session_state:
-            st.session_state['generated'] = ["Hello ! Ask me anything about " + uploaded_file.name + " 🤗"]
+        st.session_state['generated'] = ["Hello ! Ask me anything about " + uploaded_file.name + " 🤗"]
 
     if 'past' not in st.session_state:
-            st.session_state['past'] = ["Hey ! 👋"]    
+        st.session_state['past'] = ["Hey ! 👋"]
         
-#container for the chat history
-response_container = st.container()
-#container for the user's text input
-container = st.container()
-with container:
-    with st.form(key='my_form', clear_on_submit=True):
-        user_input = st.text_input("Query:", placeholder="Talk about your csv data here (:", key='input')
-        submit_button = st.form_submit_button(label='Send')
-    
-    if submit_button and user_input:
-        output = conversational_chat(user_input)
-        st.session_state['past'].append(user_input)
-        st.session_state['generated'].append(output)
+    #container for the chat history
+    response_container = st.container()
+    #container for the user's text input
+    container = st.container()
+
+    with container:
+        with st.form(key='my_form', clear_on_submit=True):
+            
+            user_input = st.text_input("Query:", placeholder="Talk about your csv data here (:", key='input')
+            submit_button = st.form_submit_button(label='Send')
+            
+        if submit_button and user_input:
+            output = conversational_chat(user_input)
+            
+            st.session_state['past'].append(user_input)
+            st.session_state['generated'].append(output)
 
     if st.session_state['generated']:
         with response_container:
@@ -87,3 +90,4 @@ with container:
                 message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="big-smile")
                 message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
                 
+#streamlit run tuto_chatbot_csv.py
